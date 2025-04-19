c303794150719b38246d1c4d8c25e5ad3b204615
myd7349 <myd7349@gmail.com>
2025-04-19 19:15:57 +0800

    Fix

--- patch_cmakelists.py
+++ patch_cmakelists.py
@@ -23,6 +23,21 @@ if __name__ == "__main__":
 
     replacements = [
         (
+            "set(LSL_ABI_VERSION 2)",
+            """set(LSL_ABI_VERSION 2)
+
+if(MSVC)
+    foreach(flag_var
+        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+        if(${flag_var} MATCHES "/MD")
+            string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+        endif(${flag_var} MATCHES "/MD")
+    endforeach(flag_var)
+endif()
+"""
+        ),
+        (
             "target_link_libraries(lsl PRIVATE lslobj lslboost)",
 """
 if(MSVC AND NOT LSL_BUILD_STATIC)
--- toolchains/msvc_static_crt.cmake
+++ toolchains/msvc_static_crt.cmake
@@ -4,11 +4,3 @@ message(STATUS "CMP0091 status: ${cmp0091_status}")
 cmake_policy(SET CMP0091 NEW)
 
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE STRING "" FORCE)
-
-foreach(flag_var
-    CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-    CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-    if(${flag_var} MATCHES "/MD")
-        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-    endif(${flag_var} MATCHES "/MD")
-endforeach(flag_var)
