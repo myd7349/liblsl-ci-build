054d860e3cf8e2a082d9b12dc4c66fef79f21cbc
myd7349 <myd7349@gmail.com>
2025-04-19 18:09:46 +0800

    Set CMP0091 to NEW

--- toolchains/msvc_static_crt.cmake
+++ toolchains/msvc_static_crt.cmake
@@ -1,1 +1,6 @@
+cmake_policy(GET CMP0091 cmp0091_status)
+message(STATUS "CMP0091 status: ${cmp0091_status}")
+
+cmake_policy(SET CMP0091 NEW)
+
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" CACHE STRING "" FORCE)
